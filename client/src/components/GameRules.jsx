ad3219912a8ab9d557b284790ac123c78e4c6c6b
Marta Organisciak <marta.organisciak@wp.pl>
2023-10-10 23:06:11 +0200

    game rules

--- client/src/components/GameRules.jsx
+++ client/src/components/GameRules.jsx
@@ -6,11 +6,11 @@ export default function GameRules() {
       <div>
         <p>
           In this game you should guess the names of the cities they are
-          presented on the next pages on the photos. Click on the input window
-          and put there th name of presented city. When you ready, click on the
-          right arrow to go to the next page. At the end, you will see your
-          total score and the comparison your score with the best results of
-          other players. Good luck and have fun!
+          presented on the next pages on the photos. Click into input window and
+          put there your guess of the name of presented city. When you are
+          ready, click on the right arrow to go to the next page. At the end,
+          you will see your total score and the comparison your score with the
+          best results of other players. Good luck and have fun!
         </p>
       </div>
     </>
