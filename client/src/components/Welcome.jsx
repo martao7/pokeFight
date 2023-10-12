ae23cdea53a0859d6eeb3e20bb2a1cb5237c13bd
Marta Organisciak <marta.organisciak@wp.pl>
2023-10-12 15:09:47 +0200

    picture globe

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -20,11 +20,11 @@ export default function Welcome() {
     <>
       <div className="welcome">
         <div className="picture1">
-          <img src="./img/globe1.png" width="50%" />
-          {/* <img
-            src="https://pixabay.com/get/g72147e841bf49cd41fc9ce864248acedeeb293501995f9cfc9c12aebbf29d4f10af6828286b28b2fa327d64ceeb8b8e7826adf367760964a8a3a05c6f9043e48_1280.png"
+          {/* <img src="./img/globe1.png" width="50%" /> */}
+          <img
+            src="https://media.istockphoto.com/id/186019678/de/foto/planet-erde-mit-einigen-wolken-americas-view.jpg?s=612x612&w=0&k=20&c=5ElJXzIoB4FKIBDi0D8EVcVVfOivqU-A1SllAFvNVis="
             width="50%"
-          /> */}
+          />
           <div className=""></div>
         </div>
 
